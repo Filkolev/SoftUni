397bdddfda395730dd273210471cb6ff6519647f
Filkolev <fil.kolev@gmail.com>
2014-08-15 03:27:06 +0300

    Create Problem10.ReformatCSharpCode.cs

---
+++ CSharpBasics/Homeworks/Homework1-IntroToProgramming/Problem10.ReformatCSharpCode.cs
@@ -0,0 +1,18 @@
+using System;
+
+// Reformat the following C# code to make it readable according to the C# best practices 
+// for code formatting. Change the casing of the identifiers in the code (e.g. use PascalCase for the class name)
+
+class HorribleCode
+{
+    static void Main()
+    {
+        Console.WriteLine("Hi, I am horribly formatted program");
+        Console.WriteLine("Numbers and squares:");
+        
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(i + " --> " + i * i);
+        }
+    }
+}
