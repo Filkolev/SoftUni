676afbccdf2bcd8e15ea0bf23f4507ebcf53f502
Filkolev <fil.kolev@gmail.com>
2014-08-15 03:23:41 +0300

    Create Problem08.SquareRoot.cs

---
+++ CSharpBasics/Homeworks/Homework1-IntroToProgramming/Problem08.SquareRoot.cs
@@ -0,0 +1,11 @@
+using System;
+
+// Create a console application that calculates and prints the square root of the number 12345. 
+
+class SquareRoot
+{
+    static void Main()
+    {
+        Console.WriteLine(Math.Sqrt(12345));
+    }
+}
