51f5dda73125474452d7e3e4112eb223f0982b74
Filkolev <fil.kolev@gmail.com>
2014-08-15 03:28:10 +0300

    Create Problem14.CurrentDateAndTime.cs

---
+++ CSharpBasics/Homeworks/Homework1-IntroToProgramming/Problem14.CurrentDateAndTime.cs
@@ -0,0 +1,11 @@
+using System;
+
+// Create a console application that prints the current date and time.
+
+class CurrentDateAndTime
+{
+    static void Main()
+    {
+        Console.WriteLine(DateTime.Now);
+    }
+}
