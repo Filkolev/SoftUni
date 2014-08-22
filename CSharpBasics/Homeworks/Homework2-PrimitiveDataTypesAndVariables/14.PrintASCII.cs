20fdef205b8f26967ba6b74db79d81a68555b989
Filkolev <fil.kolev@gmail.com>
2014-08-22 20:19:19 +0300

    Update 14.PrintASCII.cs
    Minor formatting changes

--- CSharpBasics/Homeworks/Homework2-PrimitiveDataTypesAndVariables/14.PrintASCII.cs
+++ CSharpBasics/Homeworks/Homework2-PrimitiveDataTypesAndVariables/14.PrintASCII.cs
@@ -14,7 +14,7 @@ class PrintASCII
         for (int i = 0; i < 256; i++)
         {        
             char symbol = (char)i; 
-            Console.WriteLine(i+" --> "+symbol);
+            Console.WriteLine(i + " --> " + symbol);
         }
     }
 }
