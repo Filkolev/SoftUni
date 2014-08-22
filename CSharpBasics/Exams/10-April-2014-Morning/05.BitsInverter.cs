ea2744d4db77c74e531ae45ae98d2f71771ac7c9
Filkolev <fil.kolev@gmail.com>
2014-08-22 20:45:23 +0300

    Create 05.BitsInverter.cs

---
+++ CSharpBasics/Exams/10-April-2014-Morning/05.BitsInverter.cs
@@ -0,0 +1,44 @@
+using System;
+
+// Find problem here: http://judge.softuni.bg/Contests/Practice/DownloadResource/20
+
+class BitsInverter
+{
+    static void Main()
+    {              
+        int n = int.Parse(Console.ReadLine());
+        int step = int.Parse(Console.ReadLine());
+       
+        int[] bytes = new int[n];
+       
+        for (int i = 0; i < n; i++)
+        {
+            bytes[i] = int.Parse(Console.ReadLine()); 
+        }
+        
+        int position = 7;
+        
+        for (int i = 0; i < n; i++)
+        {
+            while (position >= 0)
+	        {
+	            int currentBit = (bytes[i] >> position) & 1;
+
+                if (currentBit == 0)
+                {
+                    bytes[i] = (1 << position) | bytes[i];
+                }
+                else
+                {
+                    bytes[i] = ~(1 << position) & bytes[i];
+                }
+
+                position -= step;
+	        }
+
+            Console.WriteLine(bytes[i]);
+
+            position += 8;
+        }
+    }
+}
