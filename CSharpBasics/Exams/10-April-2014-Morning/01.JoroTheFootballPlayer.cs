58a6bdf5f7d504126a173d5d784fd8af7312a805
Filkolev <fil.kolev@gmail.com>
2014-08-22 20:26:06 +0300

    Create 01.JoroTheFootballPlayer.cs

---
+++ CSharpBasics/Exams/10-April-2014-Morning/01.JoroTheFootballPlayer.cs
@@ -0,0 +1,27 @@
+using System;
+
+// Find problem here: http://judge.softuni.bg/Contests/Practice/DownloadResource/12
+
+class JoroTheFootballPlayer
+{
+    static void Main()
+    {         
+      string isLeap = Console.ReadLine();
+	    int holidays = int.Parse(Console.ReadLine());
+	    int hometownWeekends = int.Parse(Console.ReadLine());
+
+      int normalWeekends = 52 - hometownWeekends;
+      double plays = 0;        
+
+      plays += holidays/2.0; 
+      plays += (2.0 / 3) * normalWeekends; 
+      plays += hometownWeekends; 
+
+      if (isLeap == "t")
+		    {
+			    plays += 3; 
+		    }
+  
+      Console.WriteLine((int)plays);
+    }
+}
