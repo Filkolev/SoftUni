9e049a1ce7f6d4fd75afcf16e2fa7bf6fa9ae01f
Filkolev <fil.kolev@gmail.com>
2014-08-22 20:00:07 +0300

    Create 05.BooleanVariable.cs

---
+++ CSharpBasics/Homeworks/Homework2-PrimitiveDataTypesAndVariables/05.BooleanVariable.cs
@@ -0,0 +1,12 @@
+using System;
+
+// Declare a Boolean variable called isFemale and assign an appropriate value corresponding to your gender. Print it on the console.
+
+class BooleanVariable
+{
+    static void Main()
+    {
+        bool isFemale = false;
+        Console.WriteLine(isFemale);
+    }
+}
