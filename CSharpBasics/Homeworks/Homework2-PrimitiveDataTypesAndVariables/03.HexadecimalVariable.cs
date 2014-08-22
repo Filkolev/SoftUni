16fc0e57e334d248d9d10c219d34a803d3d379c9
Filkolev <fil.kolev@gmail.com>
2014-08-22 19:56:53 +0300

    Create 03.HexadecimalVariable.cs

---
+++ CSharpBasics/Homeworks/Homework2-PrimitiveDataTypesAndVariables/03.HexadecimalVariable.cs
@@ -0,0 +1,13 @@
+using System;
+
+/* Declare an integer variable and assign it with the value 254 in hexadecimal format (0x##). 
+ * Use Windows Calculator to find its hexadecimal representation. Print the variable and ensure that the result is “254”. */
+
+class HexadecimalVariable
+{
+    static void Main()
+    {
+        int hexadecimalVar = 0xFE;
+        Console.WriteLine(hexadecimalVar);
+    }
+}
