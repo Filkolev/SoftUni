4e2fdee8fa5768d05db8554bed3d33c488fd1755
Filkolev <fil.kolev@gmail.com>
2014-08-22 20:27:13 +0300

    Update 01.JoroTheFootballPlayer.cs

--- CSharpBasics/Exams/10-April-2014-Morning/01.JoroTheFootballPlayer.cs
+++ CSharpBasics/Exams/10-April-2014-Morning/01.JoroTheFootballPlayer.cs
@@ -5,23 +5,23 @@ using System;
 class JoroTheFootballPlayer
 {
     static void Main()
-    {         
-      string isLeap = Console.ReadLine();
-	    int holidays = int.Parse(Console.ReadLine());
-	    int hometownWeekends = int.Parse(Console.ReadLine());
+    {
+        string isLeap = Console.ReadLine();
+        int holidays = int.Parse(Console.ReadLine());
+        int hometownWeekends = int.Parse(Console.ReadLine());
 
-      int normalWeekends = 52 - hometownWeekends;
-      double plays = 0;        
+        int normalWeekends = 52 - hometownWeekends;
+        double plays = 0;
 
-      plays += holidays/2.0; 
-      plays += (2.0 / 3) * normalWeekends; 
-      plays += hometownWeekends; 
+        plays += holidays / 2.0;
+        plays += (2.0 / 3) * normalWeekends;
+        plays += hometownWeekends;
 
-      if (isLeap == "t")
-		    {
-			    plays += 3; 
-		    }
-  
-      Console.WriteLine((int)plays);
+        if (isLeap == "t")
+        {
+            plays += 3;
+        }
+
+        Console.WriteLine((int)plays);
     }
 }
