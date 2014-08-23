628b97e87a62676c9d03106e3e5731533c8d3268
Filkolev <fil.kolev@gmail.com>
2014-08-23 16:50:57 +0300

    Update 05.CalculateSumWithFactorialsAndPowers.cs

--- CSharpBasics/Homeworks/Homework6-Loops/05.CalculateSumWithFactorialsAndPowers.cs
+++ CSharpBasics/Homeworks/Homework6-Loops/05.CalculateSumWithFactorialsAndPowers.cs
@@ -2,7 +2,7 @@ using System;
 using System.Numerics;
 
 /* Write a program that, for a given two integer numbers n and x, 
- calculates the sum S = 1 + 1!/x + 2!/x2 + … + n!/xn. Use only one loop. 
+ calculates the sum S = 1 + 1!/x + 2!/x2 + … + n!/x^n. Use only one loop. 
  Print the result with 5 digits after the decimal point. */
 
 class CalculateSumWithFactorialsAndPowers
