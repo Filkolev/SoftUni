332ca10b05148684e5d6e4b8b80280f4107f45ff
Filkolev <fil.kolev@gmail.com>
2014-08-23 16:56:41 +0300

    Update 06.CalculateDivisionOfFactorials.cs

--- CSharpBasics/Homeworks/Homework6-Loops/06.CalculateDivisionOfFactorials.cs
+++ CSharpBasics/Homeworks/Homework6-Loops/06.CalculateDivisionOfFactorials.cs
@@ -8,18 +8,18 @@ class CalculateDivisionOfFactorials
 {
     static void Main()
     {
-        Console.Write("n = ");
-        int n;
-        while (!int.TryParse(Console.ReadLine(), out n))
-        {
-            Console.Write("The number you entered isn't valid, please re-enter.\nn =  ");
-        }
-
         Console.Write("k = ");
         int k;
-        while (!int.TryParse(Console.ReadLine(), out k))
+        while (!int.TryParse(Console.ReadLine(), out k) || k < 2 || k > 99)
         {
-            Console.Write("The number you entered isn't valid, please re-enter.\nk =  ");
+            Console.Write("The number you entered isn't valid - k should be an integer between 2 and 99, please re-enter.\nk =  ");
+        }
+
+        Console.Write("n = ");
+        int n;
+        while (!int.TryParse(Console.ReadLine(), out n) || n <= k || n > 99)
+        {
+            Console.Write("The number you entered isn't valid - n should be an integer between {0} and 99, please re-enter.\nn =  ", k + 1);
         }
 
         BigInteger result = 1;
