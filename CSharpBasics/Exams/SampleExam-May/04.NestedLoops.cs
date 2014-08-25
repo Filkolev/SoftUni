b806458caa09241dbe4f3014a76b04e5b6daae48
Filkolev <fil.kolev@gmail.com>
2014-08-25 23:13:26 +0300

    Create 04.NestedLoops.cs

---
+++ CSharpBasics/Exams/SampleExam-May/04.NestedLoops.cs
@@ -0,0 +1,73 @@
+using System;
+
+// Problem can be found here: http://judge.softuni.bg/Contests/Practice/DownloadResource/7
+
+class NestedLoops
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();        
+
+        int b = int.Parse(Console.ReadLine());
+        int c = int.Parse(Console.ReadLine());
+
+        bool results = false;
+        int countOfBulls = 0;
+        int countOfCows = 0;
+
+        if (b > 4 || c > 4)
+        {
+            Console.WriteLine("No");
+            return;
+        }
+
+        for (int guessNumber = 1111; guessNumber <= 9999; guessNumber++)
+        {
+            char[] secretNumber = { input[0], input[1], input[2], input[3] };
+            string guess = guessNumber.ToString();
+            char[] guessNumChars = { guess[0], guess[1], guess[2], guess[3] };
+
+            if (guess[1] == '0' || guess[2] == '0' || guess[3] == '0')
+            {
+                continue;
+            }         
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (guessNumChars[i] == secretNumber[i])
+                {
+                    countOfBulls++;
+                    guessNumChars[i] = '#';
+                    secretNumber[i] = '!';
+                } 
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (guessNumChars[i] == secretNumber[j])
+                    {
+                        countOfCows++;
+                        guessNumChars[i] = '#';
+                        secretNumber[j] = '!'; 
+                    }                    
+                }
+            }
+
+            if (countOfBulls == b && countOfCows == c)
+            {
+                Console.Write("{0} ",guess);
+                results = true;
+            }
+
+            countOfBulls = 0;
+            countOfCows = 0;
+        }
+
+        if (!results)
+        {
+            Console.WriteLine("No");
+        }       
+    }
+}
