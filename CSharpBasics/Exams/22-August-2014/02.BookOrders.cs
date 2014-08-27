448907c84f051d6924759c7acf3661f93f7eba1d
Filkolev <fil.kolev@gmail.com>
2014-08-27 19:25:39 +0300

    Update 02.BookOrders.cs
    Extracted a method to make the code more manageable.

--- CSharpBasics/Exams/22-August-2014/02.BookOrders.cs
+++ CSharpBasics/Exams/22-August-2014/02.BookOrders.cs
@@ -6,7 +6,7 @@ class BookOrders
     {
         int numberOfOrders = int.Parse(Console.ReadLine());
 
-        long books = 0;
+        long totalBooks = 0;
         decimal totalPrice = 0;
 
         for (int i = 0; i < numberOfOrders; i++)
@@ -15,59 +15,64 @@ class BookOrders
             int booksPerPacket = int.Parse(Console.ReadLine());
             decimal bookPrice = decimal.Parse(Console.ReadLine());
 
-            long totalBooks = numberOfPackets * booksPerPacket;
-            books += totalBooks;
+            long booksInPacket = numberOfPackets * booksPerPacket;
+            totalBooks += booksInPacket;
 
-            if (numberOfPackets >= 10 && numberOfPackets <= 19)
-            {
-                bookPrice *= 0.95m;
-            }
-            else if (numberOfPackets >= 20 && numberOfPackets <= 29)
-            {
-                bookPrice *= 0.94m;
-            }
-            else if (numberOfPackets >= 30 && numberOfPackets <= 39)
-            {
-                bookPrice *= 0.93m;
-            }
-            else if (numberOfPackets >= 40 && numberOfPackets <= 49)
-            {
-                bookPrice *= 0.92m;
-            }
-            else if (numberOfPackets >= 50 && numberOfPackets <= 59)
-            {
-                bookPrice *= 0.91m;
-            }
-            else if (numberOfPackets >= 60 && numberOfPackets <= 69)
-            {
-                bookPrice *= 0.9m;
-            }
-            else if (numberOfPackets >= 70 && numberOfPackets <= 79)
-            {
-                bookPrice *= 0.89m;
-            }
-            else if (numberOfPackets >= 80 && numberOfPackets <= 89)
-            {
-                bookPrice *= 0.88m;
-            }
-            else if (numberOfPackets >= 90 && numberOfPackets <= 99)
-            {
-                bookPrice *= 0.87m;
-            }
-            else if (numberOfPackets >= 100 && numberOfPackets <= 109)
-            {
-                bookPrice *= 0.86m;
-            }
-            else if (numberOfPackets >= 110)
-            {
-                bookPrice *= 0.85m;
-            }
+            ApplyDiscount(numberOfPackets, ref bookPrice);
 
-            decimal orderPrice = totalBooks * bookPrice;
+            decimal orderPrice = booksInPacket * bookPrice;
             totalPrice += orderPrice;
         }
 
-        Console.WriteLine(books);
+        Console.WriteLine(totalBooks);
         Console.WriteLine("{0:F2}", totalPrice);
+    }
+
+    private static void ApplyDiscount(int numberOfPackets, ref decimal bookPrice)
+    {
+        if (numberOfPackets >= 10 && numberOfPackets <= 19)
+        {
+            bookPrice *= 0.95m;
+        }
+        else if (numberOfPackets >= 20 && numberOfPackets <= 29)
+        {
+            bookPrice *= 0.94m;
+        }
+        else if (numberOfPackets >= 30 && numberOfPackets <= 39)
+        {
+            bookPrice *= 0.93m;
+        }
+        else if (numberOfPackets >= 40 && numberOfPackets <= 49)
+        {
+            bookPrice *= 0.92m;
+        }
+        else if (numberOfPackets >= 50 && numberOfPackets <= 59)
+        {
+            bookPrice *= 0.91m;
+        }
+        else if (numberOfPackets >= 60 && numberOfPackets <= 69)
+        {
+            bookPrice *= 0.9m;
+        }
+        else if (numberOfPackets >= 70 && numberOfPackets <= 79)
+        {
+            bookPrice *= 0.89m;
+        }
+        else if (numberOfPackets >= 80 && numberOfPackets <= 89)
+        {
+            bookPrice *= 0.88m;
+        }
+        else if (numberOfPackets >= 90 && numberOfPackets <= 99)
+        {
+            bookPrice *= 0.87m;
+        }
+        else if (numberOfPackets >= 100 && numberOfPackets <= 109)
+        {
+            bookPrice *= 0.86m;
+        }
+        else if (numberOfPackets >= 110)
+        {
+            bookPrice *= 0.85m;
+        }        
     }
 }
