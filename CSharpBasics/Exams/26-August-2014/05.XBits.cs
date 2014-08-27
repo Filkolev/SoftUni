36e2bc62692ceb526731f08b1ea798d7e546bfc1
Filkolev <fil.kolev@gmail.com>
2014-08-27 11:43:49 +0300

    Create 05.XBits.cs

---
+++ CSharpBasics/Exams/26-August-2014/05.XBits.cs
@@ -0,0 +1,36 @@
+using System;
+
+class XBits
+{
+    static void Main()
+    {
+        int[] numbers = new int[8];
+
+        for (int i = 0; i < 8; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+        int mask = 7;
+        int count = 0;
+
+        for (int number = 0; number <= 5; number++)
+        {
+            for (int position = 0; position <= 30; position++)
+            {
+                if (((numbers[number] & (mask << position))  >> position) == 5)
+                {
+                    if (((numbers[number + 1] & (mask << position)) >> position) == 2)
+                    {
+                        if (((numbers[number + 2] & (mask << position)) >> position) == 5)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine(count);
+    }
+}
