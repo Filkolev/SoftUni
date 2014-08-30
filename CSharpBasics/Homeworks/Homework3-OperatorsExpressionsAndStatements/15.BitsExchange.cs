ec4f9863af7376efb80dc93ecccf01f737dfecff
Filip Kolev <Filkolev@users.noreply.github.com>
2014-08-30 10:23:33 +0300

    Update 15.BitsExchange.cs

--- CSharpBasics/Homeworks/Homework3-OperatorsExpressionsAndStatements/15.BitsExchange.cs
+++ CSharpBasics/Homeworks/Homework3-OperatorsExpressionsAndStatements/15.BitsExchange.cs
@@ -1,5 +1,3 @@
-// To be optimized
-
 using System;
 
 // Write a program that exchanges bits 3, 4 and 5 with bits 24, 25 and 26 of given 32-bit unsigned integer.
@@ -8,61 +6,20 @@ class BitsExchange
 {
     static void Main()
     {
-        Console.Write("n = ");
-        uint n = uint.Parse(Console.ReadLine());
+        Console.Write("Enter a positive 32-bit integer:\nn = ");
+        uint number = uint.Parse(Console.ReadLine());
 
-        // I declare an array where I'll store all 6 bits I'm about to exchange
-        uint[] bits = new uint[6];
+        uint mask = 7;
 
-        // This loop extracts the first 3 bits
-        for (int i = 0; i < 3; i++)
-        {
-            bits[i] = (n >> (3+i)) & 1;
-        }
+        uint rightBits = (number >> 3) & mask;
+        uint leftBits = (number >> 24) & mask;
 
-        // Extract the other 3 bits
-        for (int i = 0; i < 3; i++)
-        {
-            bits[i+3] = (n >> 24 +i) & 1;
-        }
+        number = number & ~(mask << 3);
+        number = number | (leftBits << 3);
 
-        // Start exchanging the bits
-        for (int i = 0; i < bits.Length; i++)
-        {
-            // The first 3 bits go to the left
-            if (i<bits.Length/2)
-            {
-                if (bits[i] == 0)
-                {
-                    uint mask = ~(1u << i + 24);
-                    n = n & mask;
-                }
-
-                else
-                {
-                    uint mask = 1u << i + 24;
-                    n = n | mask;
-                }
-            }
-
-            // The other 3 bits are moved to the right
-            else
-            {
-                if (bits[i] == 0)
-                {
-                    uint mask = ~(1u << i);
-                    n = n & mask;
-                }
-
-                else
-                {
-                    uint mask = 1u << i;
-                    n = n | mask;
-                }
-            }
-        }
-        
+        number = number & ~(mask << 24);
+        number = number | (rightBits << 24);        
     
-    Console.WriteLine("\nWhen we exchange bits 3, 4 and 5 with bits 24, 25 and 26 in the number n we get {0}.\n", n);
+    Console.WriteLine("\nWhen we exchange bits 3, 4 and 5 with bits 24, 25 and 26 in the number n we get {0}.\n", number);
     }
 }
