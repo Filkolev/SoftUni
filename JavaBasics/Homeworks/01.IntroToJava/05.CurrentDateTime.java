99cb2966aff8bb7454da43974b41df69ff5d9edc
Filip Kolev <Filkolev@users.noreply.github.com>
2014-09-03 16:58:55 +0300

    Create 05.CurrentDateTime.java

---
+++ JavaBasics/Homeworks/01.IntroToJava/05.CurrentDateTime.java
@@ -0,0 +1,16 @@
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+public class _05_CurrentDateTime {
+	
+	public static void main(String[] args) {
+		
+	 LocalDateTime today = LocalDateTime.now(ZoneId.of("Europe/Sofia"));
+	 
+	 System.out.printf(
+			 "Today is %s, %s %s, %s. The local time is %s:%s.", 
+			 today.getDayOfWeek(), today.getMonth(), today.getDayOfMonth(), today.getYear(), today.getHour(), today.getMinute());
+	
+	}
+	
+}
