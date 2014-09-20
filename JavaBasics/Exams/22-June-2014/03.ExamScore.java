be599db4e86ef0dfe31fbaf2619c72996f862588
Filip Kolev <Filkolev@users.noreply.github.com>
2014-09-20 16:00:31 +0300

    Create 03.ExamScore.java

---
+++ JavaBasics/Exams/22-June-2014/03.ExamScore.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+import java.util.TreeMap;
+
+
+public class _03_ExamScore {
+	public static void main(String[] args) {
+		@SuppressWarnings("resource")
+		Scanner scn = new Scanner(System.in);
+
+		scn.nextLine();
+		scn.nextLine();
+		scn.nextLine();
+
+		TreeMap<Integer, TreeMap<String, Double>> entries = new TreeMap<>(); 
+
+		while (true) {
+			String inputLine = scn.nextLine();
+			if (inputLine.startsWith("-")) {
+				break;
+			}
+
+			String[] tokens = inputLine.split("[ |]+");
+
+			String name = tokens[1] + " " + tokens[2];
+			int score = Integer.parseInt(tokens[3]);
+			Double grade = Double.parseDouble(tokens[4]);
+			TreeMap<String, Double> newEntry = new TreeMap<>();
+
+			if (entries.containsKey(score)) {				
+				if (entries.get(score).containsKey(name)) {						
+					grade += entries.get(score).get(name);
+				} 
+				entries.get(score).put(name, grade);
+			} else {
+				newEntry.put(name, grade);
+				entries.put(score, newEntry);
+			}
+		}
+		
+		for (Integer entry : entries.keySet()) {
+			System.out.print(entry + " -> ");
+			System.out.print(entries.get(entry).keySet());
+			
+			Double averageDouble = 0.0;
+			int numberOfGrades = entries.get(entry).size();
+			
+			double totalGrade = 0.0;
+			
+			for (String name : entries.get(entry).keySet()) {
+				totalGrade += entries.get(entry).get(name);
+			}
+			
+			averageDouble = totalGrade / numberOfGrades;
+			System.out.printf("; avg=%.2f", averageDouble);
+			
+			System.out.println();
+		}
+	}
+}
