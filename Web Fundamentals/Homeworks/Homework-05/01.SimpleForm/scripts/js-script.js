38d06d04eab34ea19b5010afd583eab7c5104365
Filip Kolev <fil.kolev@gmail.com>
2014-10-14 14:25:55 +0300

    Added homework 5

---
+++ Web Fundamentals/Homeworks/Homework-05/01.SimpleForm/scripts/js-script.js
@@ -0,0 +1,22 @@
+﻿function checkPasswords () {
+    var pass = document.getElementById("pass").value;
+    var confirm = document.getElementById("confirm-pass").value;
+
+    var result;
+
+    if (pass.toString() != confirm.toString() && pass.length >= 4) {
+        result = "Passwords do not match!";
+        document.getElementById("register-button").setAttribute("disabled", "disabled");
+        document.getElementById("valid").setAttribute("style", "display:none");
+        document.getElementById("valid").setAttribute("background-color", "red");
+        document.getElementById("invalid").setAttribute("style", "display:inline-block");        
+    } else if (pass.length >=4) {
+        result = "Passwords match!";
+        document.getElementById("register-button").removeAttribute("disabled");
+        document.getElementById("invalid").setAttribute("style", "display:none");
+        document.getElementById("valid").setAttribute("style", "display:inline-block");
+    }
+
+    document.getElementById("pass-check").innerHTML = result;
+}
+
