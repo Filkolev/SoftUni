b5615f1a379677d246fa77f0f9fe02af50f6d4a7
Filip Kolev <Filkolev@users.noreply.github.com>
2015-01-11 21:27:26 +0200

    Optimized solution

--- PHPBasics/Exams/Exam-2014-09-05/02.ErrorLogger.php
+++ PHPBasics/Exams/Exam-2014-09-05/02.ErrorLogger.php
@@ -1,16 +1,15 @@
 <?php
-$text = $_GET['errorLog'];
-$pattern = "/Exception\s+in\s+thread\s+\".*?\"\s+java(?:\..*)*(\..+):\s+.*\s+at\s+[^.]+\.([^\(]+)\(([^:]+):(\d+)/";
-//$pattern = "/Exception in thread \".*\" java.*\.(.*):.*\n.*?\.(.*?)\((.*?):(\d+)/";
 
-preg_match_all($pattern, $text, $results);
+$pattern = "/Exception\s+in\s+thread\s+\".*?\"\s+(?:.*?\.)+(?P<exception>.*?):\s+\d+\s+at\s+.*?\.(?P<method>.*?)\((?P<file_name>.*?):(?P<line>\d+)\)/";
+preg_match_all($pattern, $_GET['errorLog'], $matches, PREG_SET_ORDER);
+
 $result = "<ul>";
 
-for ($entry = 0; $entry < count($results[0]); $entry++) {
-    $result .= "<li>line <strong>" . htmlspecialchars($results[4][$entry]) . "</strong> - <strong>";
-    $result .= htmlspecialchars(trim($results[1][$entry], ".")) . "</strong> in <em>";
-    $result .= htmlspecialchars($results[3][$entry]) . ":";
-    $result .= htmlspecialchars($results[2][$entry]) . "</em></li>";
+foreach ($matches as $match) {
+    $result .= "<li>line <strong>" . htmlspecialchars($match['line']);
+    $result .= "</strong> - <strong>" . htmlspecialchars($match['exception']);
+    $result .= "</strong> in <em>" . htmlspecialchars($match['file_name']) . ":";
+    $result .= htmlspecialchars($match['method']) . "</em></li>";
 }
 
 $result .= "</ul>";
