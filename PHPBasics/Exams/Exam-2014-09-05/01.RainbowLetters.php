f64a64954b0edbc1df8aa2ed57bfeb53402c4e20
Filip Kolev <Filkolev@users.noreply.github.com>
2015-01-11 16:35:10 +0200

    Optimized solution

--- PHPBasics/Exams/Exam-2014-09-05/01.RainbowLetters.php
+++ PHPBasics/Exams/Exam-2014-09-05/01.RainbowLetters.php
@@ -1,26 +1,21 @@
 <?php
-$text = $_GET['text'];
-$red = $_GET['red'];
-$green = $_GET['green'];
-$blue = $_GET['blue'];
-$nth = $_GET['nth'];
 
-$red = str_pad(dechex($red), 2, "0", STR_PAD_LEFT);
-$green = str_pad(dechex($green), 2, "0", STR_PAD_LEFT);
-$blue = str_pad(dechex($blue), 2, "0", STR_PAD_LEFT);
+$text = $_GET['text'];
+$red = str_pad(dechex($_GET['red']), 2, '0', STR_PAD_LEFT);
+$green = str_pad(dechex($_GET['green']), 2, '0', STR_PAD_LEFT);
+$blue = str_pad(dechex($_GET['blue']), 2, '0', STR_PAD_LEFT);
+$n = $_GET['nth'];
 
 $color = "#" . $red . $green . $blue;
-$chars = str_split($text);
-$count = 0; ?>
+$result = "<p>";
 
-<p><?php
-    foreach ($chars as $letter) {
-        $count++;
-        if ($count % $nth == 0) { ?><span style="color: <?php echo $color?>"><?php
-        }
-        echo htmlspecialchars($letter);
-
-        if ($count % $nth == 0) { ?></span><?php
-        }
+for ($i = 0; $i < strlen($text); $i++) {
+    if (($i + 1) % $n == 0) {
+        $result .= "<span style=\"color: $color\">". htmlspecialchars($text[$i]) ."</span>";
+    } else {
+        $result .= htmlspecialchars($text[$i]);
     }
-    ?></p>
+}
+
+$result .= "</p>";
+echo $result;
